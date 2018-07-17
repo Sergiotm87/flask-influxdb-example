ed0e60bba4a8f0f2ff8ee8176fb0b5cf6c5a0bec
sergiotm87 <sergiotm87@gmail.com>
2018-07-17 14:46:11 +0200

    initial files

---
+++ example.py
@@ -0,0 +1,66 @@
+from flask_influxdb import InfluxDB
+from flask import Flask, render_template
+from flask import jsonify
+
+app = Flask(__name__)
+app.config.from_pyfile('example.cfg')
+influx_db = InfluxDB(app=app)
+
+
+@app.route('/newdb/<dbname>')
+def newdb(dbname):
+    dbcon = influx_db.connection
+    dbcon.create_database(dbname)
+    return ''
+
+
+# example data
+@app.route('/<dbname>/write')
+def write(dbname):
+    data_measurement = 'testseries'
+    data_tags = ['time', 'value_1', 'value_2', 'value_3']
+
+    dbcon = influx_db.connection
+    dbcon.switch_database(database=dbname)
+    dbcon.write_points([
+        {
+            "fields": {
+                'value_1': 0.5,
+                'value_2': 1,
+                'value_3': 1.8858
+            },
+            "tags": {
+                'tag_1': 'tag_string',
+                'tag_2': 'tag_string'
+            },
+            "measurement": "testseries"
+        }
+    ])
+    tabledata = dbcon.query('SELECT {0} from {1}'.format(', '.join(data_tags), data_measurement))
+
+    data_points = []
+    for measurement, tags in tabledata.keys():
+        for p in tabledata.get_points(measurement=measurement, tags=tags):
+            data_points.append(p)
+
+    return render_template('table.html',
+                           measurement=data_measurement,
+                           columns=data_tags,
+                           points=data_points)
+
+@app.route('/<dbname>/read')
+def read(dbname):
+
+    # add name and values for measurement
+    data_measurement = 'testseries'
+    data_tags = ['time', 'value_1', 'value_2', 'value_3']
+
+    dbcon = influx_db.connection
+    dbcon.switch_database(database=dbname)
+
+    tabledata = dbcon.query('SELECT {0} from {1}'.format(', '.join(data_tags), data_measurement))
+    serie=list(tabledata.get_points(measurement=data_measurement))
+
+    return jsonify(serie)
+
+app.run()
